082ce1aa6647e7af089a0b42d2c3282e97c5db36
shmoodyy <123822764+shmoodyy@users.noreply.github.com>
2023-02-13 18:52:35 +0400

    Final.

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Decode.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Decode.java
@@ -11,7 +11,7 @@ public class Decode {
     static boolean isZero;
     static boolean validCode;
 
-    Decode () {
+    public Decode () {
         decryption = new StringBuilder();
         decodedBit = new StringBuilder();
         result = new StringBuilder();
